71f7cba7cdc5bdb70ef1b4fcf55b87d430e1493a
Talha Ashraf <talha.ashraf@brainxtech.com>
2023-04-13 14:25:47 +0500

    created ui of app

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  TemperatureConverter
+//  ViewsAndFrames
 //
 //  Created by BrainX Technologies on 13/04/2023.
 //
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        var firstView = UIView(frame: CGRect(x: 100, y: 100, width: 150, height: 150))
+//        firstView.backgroundColor = .blue
+//
+//        view.addSubview(firstView)
+//
+//
+//        var secondView = UIView(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
+//        secondView.backgroundColor = .red
+//
+//        firstView.addSubview(secondView)
     }
 
 
